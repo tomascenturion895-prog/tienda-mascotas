a17a2dc3f1293ced30b815067f7ecf504dc20877
Tomas Centurion <tomascenturion895@gmail.com>
2026-05-17 23:57:30 -0300

    Creacion de API

---
+++ tienda-mascotas/app/Http/Controllers/Api/ArticuloController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Articulo::with('categoria')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+            'precio' => 'required|numeric',
+            'categoria_id' => 'required|exists:categorias,id',
+        ]);
+
+        return Articulo::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return $articulo->load('categoria');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $articulo->update($request->all());
+        return $articulo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $articulo->delete();
+        return response()->noContent();
+    }
+}
